89ff082c4fcb37d4a5c04c94d1b2de91199fccf8
nemingway <nemingway@f31966e2-001a-11de-ada4-5153b8187bf2>
2009-02-25 13:18:12 +0000

    Fix bug in Component.js : configuration properties were sometimes erroneously shared by components (still the _definition/_extension mess...)
    Work on the demo part : code snippet

--- trunk/demo/global/syntax_highlighter.js
+++ trunk/demo/global/syntax_highlighter.js
@@ -207,3 +207,23 @@ CodeHighlighter.addStyle("javascript",{
 		exp  : /\b(toString|valueOf|window|element|prototype|constructor|document|escape|unescape|parseInt|parseFloat|setTimeout|clearTimeout|setInterval|clearInterval|NaN|isNaN|Infinity)\b/
 	}
 });
+
+CodeHighlighter.addStyle("html", {
+	comment : {
+		exp: /&lt;!\s*(--([^-]|[\r\n]|-[^-])*--\s*)&gt;/
+	},
+	tag : {
+		exp: /(&lt;\/?)([a-zA-Z]+\s?)/,
+		replacement: "$1<span class=\"$0\">$2</span>"
+	},
+	string : {
+		exp  : /'[^']*'|"[^"]*"/
+	},
+	attribute : {
+		exp: /\b([a-zA-Z-:]+)(=)/,
+		replacement: "<span class=\"$0\">$1</span>$2"
+	},
+	doctype : {
+		exp: /&lt;!DOCTYPE([^&]|&[^g]|&g[^t])*&gt;/
+	}
+});
