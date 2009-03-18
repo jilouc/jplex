58f395cce2938841b0faaab9009e41129d0a05cc
nemingway <nemingway@f31966e2-001a-11de-ada4-5153b8187bf2>
2009-03-18 23:21:40 +0000

    Big refactor and doc update on MenuBar & Co.
    + Minor bug fixes

--- trunk/src/jplex/components/menubar/MenuBarSubmenu.js
+++ trunk/src/jplex/components/menubar/MenuBarSubmenu.js
@@ -3,53 +3,26 @@
  * @description !MenuBar Submenu
  * @class menubar.MenuBarSubmenu
  * @param {MenuBar.MenuBarItem} oParent the parent item of the submenu
- * @param {Array} oItems array of sub-items (see the documentation of MenuBar)
- * @param {Integer} nLevel depth of the submenu (0 for the immediate children of root elements)
+ * @param {Array} items array of sub-items (see the documentation of MenuBar)
+ * @param {int} level depth of the submenu (0 for the immediate children of root elements)
  * @constructor
  * @static
  * @private
  */
 jPlex.provide('jplex.components.menubar.MenuBarSubmenu', {
-    /**
-     * Array of submenu items
-     * @property oItems
-     * @type Array<MenuBar.MenuBarItem>
-     * @see MenuBar.Item
-     */
 
-    /**
-     * Depth of the submenu
-     * (0 for the immediate children of root elements)
-     * @property nLevel
-     * @type Integer
-     * @private
-     */
-
-    /**
-     * Reference to the submenu's parent item
-     * @property oParent
-     * @type MenuBar.MenuBarItem
-     * @private
-     */
-
-    /**
-     * Event that fires when an item is added to the menu
-     * @event onItemAddEvent
-     * @param {MenuBar.MenuBarItem} item the added item
-     */
-
-    initialize: function(oParent, oItems, nLevel) {
-        this.nLevel = nLevel;
-        this.oParent = oParent;
-        this.oItems = $A([]);
+    initialize: function(parentItem, items, level) {
+        this._level  = level;
+        this._parent = parentItem;
+        this._items  = $A([]);
 
         this.render();
 
-        if (oItems) {
-            for (var i = 0; i < oItems.length; i++) {
-                if (!oItems[i] || !oItems[i].name)
+        if (items) {
+            for (var i = 0; i < items.length; i++) {
+                if (!items[i] || !items[i].name)
                     continue;
-                this.addItem(oItems[i]);
+                this.addItem(items[i]);
             }
         }
     },
@@ -58,17 +31,17 @@ jPlex.provide('jplex.components.menubar.MenuBarSubmenu', {
      * Renders the submenu, with shade
      */
     render: function() {
-        this.eElement = $(new Element("ul"));
+        this.me = $(new Element("ul"));
         this.eShade = $(new Element("div"));
         this.eShade.setStyle({
             background: "#000000",
-            zIndex: 2 * this.nLevel,
+            zIndex: 2 * this._level,
             position: "absolute"
         }).setOpacity(0.2);
 
-        this.eElement.addClassName("submenu").setStyle({
+        this.me.addClassName("submenu").setStyle({
             display: "none",
-            zIndex: 2 * this.nLevel + 1
+            zIndex: 2 * this._level + 1
         });
     },
 
@@ -76,8 +49,8 @@ jPlex.provide('jplex.components.menubar.MenuBarSubmenu', {
      * Makes the submenu visible
      */
     show: function() {
-        if (!this.isEmpty() && !this.get().visible()) {
-            this.get().show();
+        if (!this.isEmpty() && !this.me.visible()) {
+            this.me.show();
             this.eShade.show();
             this._fixPosition();
         }
@@ -87,12 +60,12 @@ jPlex.provide('jplex.components.menubar.MenuBarSubmenu', {
      * Hides the submenu
      */
     hide: function() {
-        if (!this.isEmpty() && this.get().visible()) {
-            this.oItems.each(function(s) {
-                s.oSubmenu.hide();
+        if (!this.isEmpty() && this.me.visible()) {
+            this._items.each(function(s) {
+                s.getSubmenu().hide();
             });
 
-            this.get().hide();
+            this.me.hide();
             this.eShade.hide();
         }
     },
@@ -101,96 +74,72 @@ jPlex.provide('jplex.components.menubar.MenuBarSubmenu', {
      * Get the `<ul>` element for the submenu
      * @return {Element} The `<ul>` container for all `<ul>` items of the submenu
      */
-    get: function() {
-        return this.eElement;
-    },
-
-    /**
-     * Calculates the right position for the submenu depending on its parent's position
-     * (Called every time the submenu is shown)
-     * @private
-     */
-    _fixPosition: function() {
-
-        var parent = this.oParent.get(),
-               pos = parent.positionedOffset();
-        var top, left;
-        if (this.nLevel == 0) {
-            top = pos.top + parent.getHeight();
-            left = pos.left;
-
-        } else {
-            top = pos.top;
-            left = pos.left + parent.getWidth();
-        }
-        this.get().style.top = top + "px";
-        this.get().style.left = left + "px";
-        this.eShade.style.top = top + "px";
-        this.eShade.style.left = (left - 2) + "px";
-        this.eShade.style.width = (this.get().getWidth() + 4) + "px";
-        this.eShade.style.height = (this.get().getHeight() + 2) + "px";
+    getHTMLElement: function() {
+        return this.me;
     },
 
     /**
      * Checks whether the submenu is empty or not
-     * @return {boolean} true if the submenu doesn't contain any item
+     * @return {bool} true if the submenu doesn't contain any item
      */
     isEmpty: function() {
-        return this.oItems.length == 0;
+        return this._items.length == 0;
     },
 
     /**
      * Adds the item oItem to the submenu.
      * If specified, adds it before the nBefore'th item
-     * @param {Object} oItem Item to add (please refer to MenuBar's description above for item properties)
-     * @param {Integer} nBefore Optional. Position of the item (by default the item is pushed at the end)
+     * @param {Object} item Item to add (please refer to MenuBar's description above for item properties)
+     * @param {int} before Optional. Position of the item (by default the item is pushed at the end)
      * @return {MenuBar.Submenu} the modified MenuBar (allows chained items adds)
      */
-    addItem: function(oItem, nBefore) {
-        var item = new jplex.components.menubar.MenuBarItem(this, oItem, this.nLevel + 1);
-        if (nBefore) {
-            this.eElement.insertBefore(item.get(), this.oItems[nBefore].get());
+    addItem: function(item, before) {
+        var item = new jplex.components.menubar.MenuBarItem(this, item, this._level + 1);
+        if (before) {
+            this.me.insertBefore(item.getHTMLElement(), this._items[before].getHTMLElement());
             var tmp = $A([]);
-            for (var i = 0; i < this.oItems.length; i++) {
-                if (i == nBefore) {
+            for (var i = 0; i < this._items.length; i++) {
+                if (i == before) {
                     tmp.push(item);
                 }
-                tmp.push(this.oItems[i]);
+                tmp.push(this._items[i]);
             }
-            this.oItems = tmp.compact();
+            this._items = tmp.compact();
         } else {
-            this.eElement.appendChild(item.get());
-            this.oItems.push(item);
+            this.me.appendChild(item.getHTMLElement());
+            this._items.push(item);
         }
-        jplex.components.MenuBar.getInstance().fireEvent("onItemAddEvent", {item:item});
+        this.getRootMenuBar().fireEvent("onItemAddEvent", {
+            item: item
+        });
         return this;
     },
 
     /**
      * Get the submenu of the item at position nIndex
-     * @param {Integer} nIndex Position of the item to get
-     * @return {MenuBar.Submenu|boolean} the submenu of the specified item, or false if no matching item was found
+     * @param {int} index Position of the item to get
+     * @return {MenuBar.Submenu|bool} the submenu of the specified item, or false if no matching item was found
      */
-    getItem: function(nIndex) {
-        var item = this.oItems[nIndex];
+    getItem: function(index) {
+        var item = this._items[index];
         if (item) {
-            return this.oItems[nIndex].getSubmenu();
+            return this._items[index].getSubmenu();
         } else {
             return false;
         }
     },
 
     /**
-     * Removes the nIndex-th item of the submenu
-     * @param {Integer} nIndex position of the item to remove
-     * @return {MenuBar.Submenu|boolean} false if the item doesn't exist, else the modified menu
+     * Removes the index-th item of the submenu
+     * @param {int} index position of the item to remove
+     * @return {menubar.MenuBarSubmenu|boolean} false if the item doesn't exist, else the modified menu
      */
-    removeItem: function(nIndex) {
-        var item = this.getItem(nIndex);
+    removeItem: function(index) {
+        var item = this.getItem(index);
         if (item) {
-            this.eElement.removeChild(item.getParentItem().get());
-            delete this.oItems[nIndex];
-            this.oItems = this.oItems.compact();
+            this.me.removeChild(item.getParentItem().getHTMLElement());
+            delete this._items[index];
+            this._items = this._items.compact();
             return this;
         } else {
             return false;
@@ -199,9 +148,103 @@ jPlex.provide('jplex.components.menubar.MenuBarSubmenu', {
 
     /**
      * Get the parent item of the submenu
-     * @return {MenuBar.Item} the parent of the submenu
+     * @return {menubar.MenuBarItem} the parent of the submenu
      */
     getParentItem: function() {
-        return this.oParent;
+        return this._parent;
+    },
+
+    /**
+     * Returns a reference to the root menu bar
+     * @return {MenuBar} the root menu bar of this submenu 
+     */
+    getRootMenuBar: function() {
+        var parent = this.getParentItem().getMenu();
+        if(parent._level == 0) {
+            return parent.getParentItem().getMenu();
+        } else {
+            return parent.getRootMenuBar();
+        }
+    },
+
+    /**
+     * Returns the submenu items (array)
+     * @return {Array} an array of the submenu items (menubar.MenuBarItem)
+     */
+    getItems: function() {
+        return this._items;
+    },
+
+
+    //---------- Private methods ----------
+
+    /**
+     * Calculates the right position for the submenu depending on its parent's position
+     * (Called every time the submenu is shown)
+     * @private
+     */
+    _fixPosition: function() {
+
+        var parent = this.getParentItem().getHTMLElement();
+        var pos = parent.positionedOffset();
+        var top, left;
+
+        if (this._level == 0) {
+            top = pos.top + parent.getHeight();
+            left = pos.left;
+
+        } else {
+            top = pos.top;
+            left = pos.left + parent.getWidth();
+        }
+        this.me.setStyle({
+            top: top + "px",
+            left: left + "px"
+        });
+        this.eShade.setStyle({
+            top: top + "px",
+            left: (left - 2) + "px",
+            width: (this.me.getWidth() + 4) + "px",
+            height: (this.me.getHeight() + 2) + "px"
+        });
     }
+
+
+    //---------- Private properties ----------
+
+    /**
+     * Array of submenu items
+     * @property _items
+     * @type Array<MenuBar.MenuBarItem>
+     * @see menubar.Item
+     */
+
+    /**
+     * Depth of the submenu
+     * (0 for the immediate children of root elements)
+     * @property _level
+     * @type int
+     * @private
+     */
+
+    /**
+     * Reference to the submenu's parent item
+     * @property _parent
+     * @type menubar.MenuBarItem
+     * @private
+     */
+
+    /**
+     * The shadow HTML element for the submenu
+     * @property _shadow
+     * @type Element
+     * @private
+     */
+
+    /**
+     * The `<ul>` HTML Element of the submenu
+     * @property me
+     * @type Element
+     * @private 
+     */
 });
